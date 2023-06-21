97b3aabc10c92ca8404fc5aa59c6418a6913b50e
JiangHai <hanayo@kayotin.cn>
2023-06-21 17:57:32 +0800

    111

--- 111.py
+++ 111.py
@@ -1,29 +1,23 @@
-import pathlib
+
+
 import win32com.client as win32
+import os
+def transform(parent_path,out_path):
+    fileList = os.listdir(parent_path)  #文件夹下面所有的文件
+    num = len(fileList)
+    for i in range(num):
+        file_Name = os.path.splitext(fileList[i])   #文件和格式分开
+        if file_Name[1] == '.xls':
+            transfile1 = parent_path+'\\'+fileList[i]  #要转换的excel
+            transfile2 = out_path+'\\'+file_Name[0]    #转换出来excel
+            excel=win32.gencache.EnsureDispatch('excel.application')
+            pro=excel.Workbooks.Open(transfile1)   #打开要转换的excel
+            pro.SaveAs(transfile2 + ".xlsx", FileFormat=51)  # 另存为xlsx格式
+            pro.Close()
+            excel.Application.Quit()
+def
 
-
-def trans_xlsx():
-    """将Excel转换成xlsx"""
-    file_name = input("输入文件路径：")
-
-    # 创建 Excel Application 对象
-    excel = win32.gencache.EnsureDispatch('Excel.Application')
-
-    try:
-        # 使用 with 语法打开 .xls 文件
-        with excel.Workbooks.Open("res/exp.xls") as wb:
-            # 将 .xls 文件另存为 .xlsx 文件
-            wb.SaveAs(f'transed.xlsx', FileFormat=51)
-
-        print('转换成功！')
-
-    except Exception as e:
-        print(f'转换失败！原因：{e}')
-
-    finally:
-        # 退出 Excel
-        excel.Application.Quit()
-
-
-
-trans_xlsx()
+if __name__=='__main__':
+    path1=r"E:\untitled1\test_report_demo"  #待转换文件所在目录
+    path2=r"E:\untitled1\test_data"  #转换文件存放目录
+    transform(path1, path2)
